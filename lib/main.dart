3ced17d8aaa8e93e3071a75aec636c5d33692492
maheshwar ravuri <mravuri96@gmail.com>
2019-02-16 10:23:38 -0800

    removed login page as it has scaling issues

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: FloatActBttn(),
       routes: routes,
     );
   }
