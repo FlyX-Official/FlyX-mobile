fdcbcaa1636bb42159be04af703811d9e92ede57
Fernando Molina Rodriguez <fer.molina.rdz@gmail.com>
2019-03-25 20:39:00 -0700

    upgrade on Settings page

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -39,10 +39,58 @@ class _Settings extends State<Settings> {
     return ListView(
       children: <Widget>[
         Container(
+          margin: const EdgeInsets.only(top: 10.0),
+          width: 90.0,
+          height: 190.0,
+          decoration: new BoxDecoration(
+              border: Border(bottom: BorderSide()),
+              shape: BoxShape.circle,
+              image: new DecorationImage(
+                  //fit: BoxFit.fill,
+
+                  image: AssetImage('lib/assets/images/Logo.png'))),
+        ),
+        Container(
           padding: new EdgeInsets.all(32.0),
           child: new Center(
             child: new Column(
               children: <Widget>[
+                ListTile(
+                  title: Text('USER SETTINGS'),
+                ),
+                ListTile(
+                  title: const Text(
+                    "My Account",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                        decoration: TextDecoration.underline),
+                  ),
+                  onTap: () {/* redirect to terms and conditions page */},
+                ),
+                ListTile(
+                  title: const Text(
+                    "Privacy & Safety",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                        decoration: TextDecoration.underline),
+                  ),
+                  onTap: () {/* redirect to terms and conditions page */},
+                ),
+              ],
+            ),
+          ),
+          decoration: BoxDecoration(border: Border(bottom: BorderSide())),
+        ),
+        Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('APP SETTINGS'),
+                ),
                 new SwitchListTile(
                   value: _mode,
                   onChanged: _onChangedMode,
@@ -50,20 +98,50 @@ class _Settings extends State<Settings> {
                       style: new TextStyle(
                           fontWeight: FontWeight.bold, color: Colors.black)),
                 ),
-                new ListTile(
+                ListTile(
                   title: const Text(
-                    "Terms and Conditions",
+                    "Notifications",
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.blueAccent,
                         decoration: TextDecoration.underline),
                   ),
                   onTap: () {/* redirect to terms and conditions page */},
-                )
+                ),
               ],
             ),
           ),
           decoration: BoxDecoration(border: Border(bottom: BorderSide())),
+        ),
+        Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(children: <Widget>[
+              ListTile(
+                title: Text('APP INFORMATION'),
+              ),
+              ListTile(
+                title: const Text(
+                  "Terms and Conditions",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                      decoration: TextDecoration.underline),
+                ),
+                onTap: () {/* redirect to terms and conditions page */},
+              ),
+              ListTile(
+                title: const Text(
+                  'About ',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                      decoration: TextDecoration.underline),
+                ),
+                onTap: () {/* redirect to terms and conditions page */},
+              )
+            ]),
+          ),
         ),
       ],
     );
